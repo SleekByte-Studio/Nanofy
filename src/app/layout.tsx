5ca6250cd303f5c6db320c1f74667065e4cd4f5b
marvelxcodes <ramakrishnan0046@gmail.com>
2024-05-10 02:44:03 +0530

    Added Necessary Tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
 import './globals.css';
+import { manrope } from '@/fonts';
 import type { Metadata } from 'next';
 import { PropsWithChildren } from 'react';
-import { manrope } from '@/fonts';
 
 export const metadata: Metadata = {
 	title: 'Nanofy',
-	description: 'Shorten Your URLs'
+	description: 'Shorten Your URLs',
+	keywords: 'url shortener, earn money online, make money, money earning app'
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang='en'>
+			<head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				/>
+			</head>
 			<body style={manrope.style}>{children}</body>
 		</html>
 	);
